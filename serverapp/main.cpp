5118361013aec70e840909f75ba1a120bc6c594a
Jacob Dawid <jacob@omg-it.works>
2015-02-12 22:18:17 +0100

    Added simple example.

--- serverapp/main.cpp
+++ serverapp/main.cpp
@@ -1,12 +1,24 @@
 #include <QCoreApplication>
+#include <QFile>
 
-#include "multithreadedtcpserver.h"
+#include "tcp/tcpmultithreadedserver.h"
+#include "http/httpwebengine.h"
+#include "http/httpiodeviceresource.h"
+
+using namespace QtWebServer;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QtWebServer::MultithreadedTcpServer s;
+    Tcp::MultithreadedServer s;
+    Http::WebEngine w;
+
+    w.addResource(new Http::IODeviceResource(
+                      "/test",
+                      new QFile("/home/jacob/text.html")));
+
+    s.setResponder(&w);
     s.listen(QHostAddress::Any, 3000);
     return a.exec();
 }
