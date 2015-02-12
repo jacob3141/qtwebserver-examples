618fbd4d0fed0aaed5ffa09091218fa840e5b56f
Jacob Dawid <jacob@omg-it.works>
2015-02-12 23:30:59 +0100

    Added content type.

--- serverapp/main.cpp
+++ serverapp/main.cpp
@@ -14,9 +14,11 @@ int main(int argc, char *argv[])
     Tcp::MultithreadedServer s;
     Http::WebEngine w;
 
-    w.addResource(new Http::IODeviceResource(
-                      "/test",
-                      new QFile("/home/jacob/text.html")));
+    Http::IODeviceResource *r = new Http::IODeviceResource(
+                "/test",
+                new QFile("/home/jacob/text.html"));
+    r->setContentType("text/html");
+    w.addResource(r);
 
     s.setResponder(&w);
     s.listen(QHostAddress::Any, 3000);
